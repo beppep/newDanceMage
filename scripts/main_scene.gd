210fd3142acfc9fb56f6c9f9a982923d0aee900f
CodeyBoi <hannes.ryberg00@gmail.com>
2025-10-18 15:26:28 +0200

    Make Phase variable enum

--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -3,22 +3,23 @@ extends Node
 @onready var units = $Units
 @onready var player = units.get_node("Player")
 
-var state := "player_movement"  # can be "player_movement", "spellcasting", "enemy??"
+enum Phase { PLAYER_MOVEMENT, SPELLCASTING, ENEMY }
 
+var state := Phase.PLAYER_MOVEMENT
 
 func start_player_turn():
-	state = "player_movement"
+	state = Phase.PLAYER_MOVEMENT
 	print("Player's turn!")
 	player.start_turn()
 
 func _process(_delta):
 	# Wait until player finishes turn, then switch
-	if state == "player_movement" and player.phase == player.Phase.NOT_MY_TURN:
+	if state == Phase.PLAYER_MOVEMENT and player.phase == player.Phase.NOT_MY_TURN:
 		start_player_turn()
 
 
 func start_enemy_turn():
-	state = "enemy"
+	state = Phase.ENEMY
 	print("Enemies' turn!")
 	await run_enemy_turns()
 	start_player_turn()
